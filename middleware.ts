f431276332c6ff96cd2304dc185c69ff105639f9
VncsSz <84037924+VncsSz@users.noreply.github.com>
2024-12-12 11:33:12 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(["/"])
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect()
-  }
-},
+export default clerkMiddleware(
+  (auth, req) => {
+    // strategy to protect all routes except public routes
+    if (isPublicRoute(req)) return // if it's a public route, do nothing
+
+
+    auth().protect() // for any other route, require auth
+  },
 (req) => ({
   clockSkewInMs: 10000,
 }),)
