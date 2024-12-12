243e84857385648a00a3f48c586dad3f7634c7e5
VncsSz <84037924+VncsSz@users.noreply.github.com>
2024-12-12 10:48:05 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,9 @@ export default clerkMiddleware((auth, request) => {
 
 export const config = {
   matcher: [
-    '/((?!_next/static/|_next/image|favicon.ico|manifest.json|icons/).*)',
-    '/api/:path*',
-    '/trpc/:path*',
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|manifest\\.json)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 }
